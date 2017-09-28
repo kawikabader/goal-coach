cd7cf61fb9a9142e358a5f30ea7b303353e750dd
Kawika Bader <ekbader@gmail.com>
2017-09-28 07:29:41 -0700

    Added firebase config. Imported firebase authentication into index file that execites on auth state change.

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyDSs1V9zisBLSJUnJMu0twUTjZz8KthhAI",
+  authDomain: "goal-coach-6541e.firebaseapp.com",
+  databaseURL: "https://goal-coach-6541e.firebaseio.com",
+  projectId: "goal-coach-6541e",
+  storageBucket: "",
+  messagingSenderId: "277816994560"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, browserHistory } from "react-router";
+import { firebaseApp } from "./firebase";
 
 import App from "./components/App.jsx";
 import SignIn from "./components/SignIn.jsx";
 import SignUp from "./components/SignUp.jsx";
+
+firebaseApp.auth().onAuthStateChanged(user => {
+  if (user) {
+    console.log("User has signed in or up", user);
+  } else {
+    console.log("User has signed out or still needs to sign in.");
+  }
+});
 
 ReactDOM.render(
   <Router path="/" history={browserHistory}>
